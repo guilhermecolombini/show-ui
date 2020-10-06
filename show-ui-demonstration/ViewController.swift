7635fca1d6064c351aad5f4190f31275afc6affa
Guilherme Colombini <guilherme.colombini@ab-inbev.com>
2020-10-06 08:44:38 -0300

    setup project

---
+++ show-ui-demonstration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  show-ui-demonstration
+//
+//  Created by Guilherme Colombini on 05/10/20.
+//  Copyright © 2020 Guilherme Colombini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ show-ui/show_ui.h
@@ -0,0 +1,19 @@
+//
+//  show_ui.h
+//  show-ui
+//
+//  Created by Guilherme Colombini on 05/10/20.
+//  Copyright © 2020 Guilherme Colombini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for show_ui.
+FOUNDATION_EXPORT double show_uiVersionNumber;
+
+//! Project version string for show_ui.
+FOUNDATION_EXPORT const unsigned char show_uiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <show_ui/PublicHeader.h>
+
+
